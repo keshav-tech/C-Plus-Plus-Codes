9a731ba7921e7afdb93ec54f22df3204903a7fa1
keshav-tech <115290128+keshav-tech@users.noreply.github.com>
2022-10-08 00:35:41 +0530

    Add files via upload

---
+++ Intro C++ Codes/Hello!-1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"Hi, My Name is Keshav Sharma" <<endl;
+    return 0;
+}
---
+++ Intro C++ Codes/Sum-2-values.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int value1;
+    cin>>value1;
+
+    int value2;
+    cin>>value2;
+
+    int sum=value1+value2;
+    cout<<sum<<endl;
+    
+    return 0;
+}
