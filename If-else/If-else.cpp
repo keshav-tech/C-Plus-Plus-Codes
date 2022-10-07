163e180b392df0290a201f09f79a789ee4f2a27a
keshav-tech <115290128+keshav-tech@users.noreply.github.com>
2022-10-08 00:38:35 +0530

    Add files via upload

---
+++ If-else/If-else.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x,y,z;                                       // These are those char on which we will operate
+    cin>>x>>y>>z;                                    // input these char
+
+    if(x>y){                                         //If x is greater than y
+        if(x>z){                                     // If x is greater than z
+            cout<<x<<endl;                           // print x
+        }
+        else{                                        //otherwise
+            cout<<z<<endl;                           //print z
+        }
+    }
+    else{                                            // otherwise
+        if(y>z){                                         // If y is greater than z
+            cout<<y<<endl;                                   // print y
+        }
+        else{                                           // otherwise
+            cout<<z<<endl;                            // print z
+        }
+    } 
+    
+    return 0;
+}
