fb1137972c7a2cb01dd916e35a0f5dc63a5f8261
Keshav Sharma <115290128+keshav-tech@users.noreply.github.com>
2022-10-29 14:26:52 +0530

    Add files via upload

---
+++ String/String/String.cpp
@@ -0,0 +1,121 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+
+    // string str;
+    // cin>> str;
+    // cout<<str;                               // Type 1
+
+
+
+    // string str;
+    // string str1(5, 'n');
+    // cout<< str1 << endl;
+    // return 0;                               //Type 2 (To print single given char. for given time )
+
+
+
+
+    // string str;
+    // getline(cin, str);
+    // cout << str << endl;                   // Type 3 (to get output of given input char. in space)
+
+
+
+    
+    // string s1 = "Fam";
+    // string s2 = "ily";  
+    // s1.append(s2); Or s1 = s1 + s2;
+    // cout << s1 << endl;                           // Type 3 (To add 2 char. together)
+
+    // cout << s1[1] << endl;                       //to get particular single character
+
+    
+   
+   
+
+    // string abc = " djdgwudhdbbsa siyrb djfhbjdk";
+    // abc.clear();                            
+    // cout << abc;                              // Type 4 (to clear the all characters)
+
+
+
+
+    // string s1 = "abc";                    
+    // string s2 = "abc";
+    // cout << s2.compare(s1) << endl;              //  Type 5(to compare the 2 values)
+
+
+    
+
+    // string s1 = "abc";                                              
+    // cout << s1 << endl;  
+    // if(!s1.empty())                           
+    // cout<<"string is  not empty";                     // Type 6 (If string is not empty)
+
+
+
+
+    // string s1 = "nincompoop";                   
+    // s1.erase(3, 2);
+    // cout << s1 << endl;                               //Type 7 (erase the char.)
+
+
+
+    // string s1 = "nincompoop";                    
+    // cout << s1.find("com") << endl;                     //Type 8 (To find the particular character)
+
+
+
+
+    // string s1 = "nincompoop";                    
+    // s1.insert(2, "lol");
+    // cout << s1 << endl;                                     // Type 9 (to insert the char.)
+
+
+
+   
+    // string s1 = "nincompoop";                     
+    // cout << s1.size() << endl;                     // to get the size of chars.
+    // for(int i=0; i<s1.length(); i++)               // to print the char. acc. to their length
+    // cout << s1[i] << endl;                         // Type 10  
+
+
+    
+
+    // string s1 = "nincompoop";
+    // string s = s1.substr(6, 4);                        
+    // cout << s << endl;                                        // Type 11(Substrings of strings)
+
+
+
+    // string s1 = "998";                                
+    // int x = stoi(s1);
+    // cout << x+2 << endl;                                 // Type 12(str to int)
+
+
+
+
+    // int x = 998;                                       
+    // cout << to_string(x) + "2" << endl;                  // Type 13(int to str which is working as a append)
+
+
+    // string s1 = "ckhuvjjbjlbjbjl";                  
+
+    // sort (s1.begin(), s1.end());
+
+    // cout << s1 << endl;                                    //Type 15 (sorting)
+
+
+
+    // string str = "KeshavSharma";
+    // string str1(5, 'k');            // To print only input value for 5 times or less/more
+    // cout<< str1 << endl;
+    // cin >> str;
+    // string str;                                               //Type 16
+    return 0;
+}
